6be2763ed88bbcb7c3ba8310ca08e1c7034e58fb
seraf <seraf@DESKTOP-A7VDQDM>
2023-10-29 18:14:11 +0300

    Добавьте файлы проекта.

---
+++ Part_02/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace part_02
+{
+    class Programm
+    {
+        static void Main()
+        {
+            int[] list = { 1, 2, 3, 4, 5, };
+            MyList<int> list2 = new MyList<int>(list);
+            Console.WriteLine(list2.Count);
+            for (int i = 0; i < list2.Count; i++) { Console.Write(list2[i].ToString().PadRight(3)); }
+            Console.WriteLine();
+
+            list2.Add(6);
+            Console.WriteLine(list2.Count);
+            for (int i = 0; i < list2.Count; i++) { Console.Write(list2[i].ToString().PadRight(3)); }
+            Console.WriteLine();
+        }
+    }
+    public class MyList<T>
+    {
+        T[]? list;
+        private int _count;
+        public int Count { get { return _count; } }
+        public MyList() { _count = 0; list = null; }
+        public MyList(T[]? list) { _count = list.Length; this.list = list; }
+        public T this[int index] 
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                this.list[index] = value;
+            }
+        }
+        public void Add(T item)
+        {
+            _count++;
+            T[] tmp = new T[list.Length + 1]; 
+            for (int i = 0; i < list.Length; i++) { tmp[i] = list[i]; }
+            tmp[list.Length] = item;
+            this.list = tmp;
+        }
+        public void Clear() { _count= 0; list = null; }
+    }
+}
---
+++ Part_03/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+
+namespace part_02
+{
+    class Programm
+    {
+        static void Main()
+        {
+
+        }
+    }
+    public class MyDictionary<TKey, TValue> //: IEnumerable<T>
+    {
+        private TKey[]? _key;
+        private TValue[]? _value;
+        //private Entry<TKey, TValue>[]? _dictionary;
+        private int _count;
+        public int Count { get { return _count; } }
+        MyDictionary() { _count = 0; _key = null; _value = null; }
+        MyDictionary(int size) { _count = size; _key = new TKey[size]; _value = new TValue[size]; }
+        MyDictionary(MyDictionary<TKey,TValue> list) { _count = list.Count; _key = list._key; _value = list._value; }
+        public TKey? key(int index)
+        {
+            return _key[index];
+        }
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return this[key];
+            }
+            set
+            {
+                this[key] = value;
+            }
+        }
+        public void Add(TKey key, TValue value) 
+        { 
+            for (int i = 0; i < _key.Length; i++)
+            {
+                if (Equals(key, _key[i])) { throw new ArgumentException("This key is already use"); }
+            }
+            MyDictionary<TKey, TValue> tmp = new MyDictionary<TKey, TValue>(_count + 1);
+            for (int i = 0; i < _count; i++)
+            {
+                tmp._key[i] = _key[i];
+                tmp._value[i] = _value[i];
+            }
+            tmp._key[_count] = _key[_count];
+            tmp._value[_count] = _value[_count];
+            _count++;
+        }
+        //public IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return (IEnumerator)this;
+        //}
+    }
+}
